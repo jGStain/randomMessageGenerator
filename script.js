b8d78e40d58b8e3c89bd86da5865b4891f22a1d7
Jason Stainer <106974811+jGStain@users.noreply.github.com>
2022-06-06 14:36:11 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+let val = 1
+val = Math.floor(Math.random() * 10) + 1 /* Generates number between 1 and 10 when clicking the button*/
+    switch (val) {
+        case 1:
+            console.log('You should do some work')
+            break;
+        case 2:
+            console.log('Make a coffee')
+            break;
+        case 3:
+            console.log('Time to work out')
+            break;
+        case 4:
+            console.log('Take a break')
+            break;
+        case 5:
+            console.log('Keep going, you can do this!')
+            break;
+        case 6:
+            console.log('Behave yourself!')
+            break;
+        case 7:
+            console.log('Treat yourself')
+            break;
+        case 8:
+            console.log('Hard work will pay off, Always!')
+            break;
+        case 9:
+            console.log('Time to eat some food')
+            break;
+        default:
+            console.log('Well done, you are smashing it!')
+            break;
+    };
+
+
